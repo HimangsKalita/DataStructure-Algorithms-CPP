f19b00a68c8972769819ebcc05ef199666bb5533
Himangshu Kalita <himangshuk13@gmail.com>
2022-02-08 21:56:07 +0530

    Initial commit

---
+++ Library/Random/C++_boilerPlate.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll int long long
+#define F first
+#define S second
+#define ci(x)	scanf("%d",&x)
+#define cl(x)	scanf("%lld",&x)
+#define cp(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define fo(i,n) for(i=0;i<n;i++)
+#define fociar(i,n) for(i=0;i<n;i++) cin>>arr[i]
+#define fopiar(i,n) for(i=0;i<n;i++) cout<<arr[i]<<"\n"
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define PI 3.1415926535897932384626
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    
+
+    return 0;
+
+}
